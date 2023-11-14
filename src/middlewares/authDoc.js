d452a350d48109c4f06c7d105ffa4cd7f4f764dd
Rick Soares <146774911+Ricksrss@users.noreply.github.com>
2023-11-13 23:40:41 -0300

    Algumas correções no codigo

--- src/middlewares/authDoc.js
+++ src/middlewares/authDoc.js
@@ -1,38 +1,37 @@
 async function authDocProducao(req, res, next) {
   const { senhaDigitada } = req.body;
 
+  // Verifica se está rodando localmente ou se a rota não é /doc/
   if (req.headers.host.includes("localhost") || req.originalUrl !== "/doc/") {
     return next();
   }
 
+  // Verifica se a senha fornecida está correta
   if (senhaDigitada === process.env.SWAGGER_SENHA_DOC) {
     return next();
   }
 
+  // Se a senha foi fornecida, mas está incorreta
   if (senhaDigitada) {
-    res.status(401).set("Content-Type", "text/html");
-    res.send(
-      Buffer.from(`
-    <form method= "post">
-    <p style= "color: red;"> Senha Errada!</p>
-    <label for = "senha">Senha da documentação: </label>
-    <input type="password"name= "senhaDigitada" id="senhaDigitada"/>
-    <button type= "submit">Entrar</button>
-    </form>
-    `)
-    )
-  } else {
-    res.status(200).set("Content-Type", "text/html");
-    res.send(
-      Buffer.from(`
-    <form method= "post">
-    <label for = "senha">Senha da documentação: </label>
-    <input type="password"name= "senhaDigitada" id="senhaDigitada"/>
-    <button type= "submit">Entrar</button>
-    </form>
-    `)
-    )
+    return res.status(401).send(`
+      <form method="post">
+        <p style="color: red;">Senha Errada!</p>
+        <label for="senha">Senha da documentação:</label>
+        <input type="password" name="senhaDigitada" id="senhaDigitada"/>
+        <button type="submit">Entrar</button>
+      </form>
+    `);
   }
+
+  // Se a senha não foi fornecida
+  return res.status(200).send(`
+    <form method="post">
+      <label for="senha">Senha da documentação:</label>
+      <input type="password" name="senhaDigitada" id="senhaDigitada"/>
+      <button type="submit">Entrar</button>
+    </form>
+  `);
 }
 
 module.exports = authDocProducao;
+
